35b469d58f3b250770d728fec9448e468e8c6f2a
Dat Lyong <mrlinlyong@gmail.com>
2022-05-09 08:45:19 +0700

    first commit

---
+++ demo.js
@@ -0,0 +1,11 @@
+inputA = prompt("Điểm Lý:")
+inputB = prompt("Điềm Hoá:")
+inputC = prompt("Điềm Sinh:")
+let a= parseInt(inputA)
+let b = parseInt(inputB)
+let c = parseInt(inputC)
+let avg = ((a+b+c)/3)
+document.write("Điểm Trung Bình:"+ avg.toFixed(1))
+
+
+
